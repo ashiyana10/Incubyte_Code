89a41cf6c006e5720affca5b0196c3dcb9d7e98c
ashiyana10 <ashiyanahalvadiya123@gmail.com>
2024-12-24 18:55:42 +0530

    Add method to handle single number

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,5 +1,6 @@
 export function add(numbers: string): number {
-    if (!numbers) return 0;
+    if (!numbers) return 0; //empty string
+    if (!numbers.includes(",")) return parseInt(numbers, 10); //single number
     return 0; // Placeholder for passing the test.
   }
   
